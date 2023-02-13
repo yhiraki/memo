c89c6e272403968cf1a4048d37cd4665f04aacfc
yhiraki <yuta@yuta-pc>
2023-02-13 16:02:54 +0900

    update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 
 rm -rf dist
 mkdir dist
-find ./ -name '*.org' -type f -print0 |
-  xargs -0 -r -I{} bash -c 'pandoc {} -o "$(echo {} | sed -e s/.org/.md/ -e s/src/dist/)"'
+find ./ -name '*.org' -type f | while read line ; do
+  dst="$(echo $line | sed -e s/.org/.md/ -e s/src/dist/)"
+  mkdir -p $(dirname $dst)
+  pandoc $line -o $dst
+done
