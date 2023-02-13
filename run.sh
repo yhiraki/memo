6e378fa338de3cb004ddd65ab1de0ce2562b6469
yhiraki <yuta@yuta-pc>
2023-02-13 16:12:22 +0900

    only find src

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 rm -rf dist
 mkdir dist
-find ./ -name '*.org' -type f | while read line ; do
+find ./src -name '*.org' -type f | while read line ; do
   dst="$(echo $line | sed -e s/.org/.md/ -e s/src/dist/)"
   mkdir -p $(dirname $dst)
   pandoc $line -o $dst
