c12d5ebb35c1b1e1d1756247d57c4e9d8031666f
yhiraki <coffexpr@gmail.com>
2023-02-16 02:21:11 +0900

    rm run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf dist
-mkdir dist
-find ./src -name '*.org' -type f | while read line ; do
-  dst="$(echo $line | sed -e s/.org/.md/ -e s/src/dist/)"
-  mkdir -p $(dirname $dst)
-  pandoc $line -o $dst
-done
