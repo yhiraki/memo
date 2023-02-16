00e7426b5db5f085a80bb6abfa4eb5fe61ab2740
yhiraki <coffexpr@gmail.com>
2023-02-16 09:52:43 +0900

    htmlize

--- publish.el
+++ publish.el
@@ -4,9 +4,13 @@
 (setq org-publish-project-alist
 	  '(("Memo"
 		 :base-directory "./src"
+		 :html-doctype "html5"
+		 :htmlized-source t
 		 :publishing-directory "./dist"
 		 :publishing-function org-html-publish-to-html
 		 :recursive t
+		 :section-numbers nil
 		 :with-author nil
+		 :with-timestamps nil
 		 )))
 (org-publish-project "Memo")
