b91d5d9f8b861549773cc9fe5a14b9b09300cace
yhiraki <coffexpr@gmail.com>
2023-02-28 14:42:21 +0900

    add static files and color theme (dark/light)

--- publish.el
+++ publish.el
@@ -10,24 +10,26 @@
 		 :with-author nil
 		 :with-date nil
 		 :with-timestamps nil
-		 :with-toc t
-		 )))
+		 :with-toc t)
+		("static"
+		 :base-directory "./static"
+		 :base-extension "css\\|js"
+		 :publishing-directory "./dist/static"
+		 :publishing-function org-publish-attachment
+		 :recursive t)
+		))
 (setq org-publish-use-timestamps-flag nil)
 (setq org-html-postamble t)
 (setq org-html-head-include-scripts nil
       org-html-head-include-default-style nil
 	  org-html-head "\
 <link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
-<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/tokyo-night-dark.min.css\">\
+<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/tokyo-night-dark.min.css\" id=\"hljs-css\">\
 ")
 (setq org-html-postamble-format
 	  '(("en" "\
 <p class=\"date\">Date: %d</p>
 <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js\"></script>
-<script>
-  document.querySelectorAll('.src').forEach((el) => {
-    hljs.highlightElement(el);
-  });
-</script>\
+<script src=\"static/js/main.js\"></script>
 ")))
-(org-publish-project "Memo")
+(org-publish-all)
