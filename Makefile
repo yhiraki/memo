edf61b61f2094e3d94c1c07a7493ddebc81c41bb
yhiraki <coffexpr@gmail.com>
2023-02-28 12:19:31 +0900

    use highlight.js instead of htmlize

--- Makefile
+++ Makefile
@@ -1,12 +1,6 @@
 EMACS ?= emacs
 
-build: site_lisp/htmlize.el
+build:
 # emacs --batch is the one that inhibits the colorization of the code snippets.
 # so use -nw
 	TERM=xterm-256color $(EMACS) -Q -nw -l publish.el -f kill-emacs
-
-site_lisp:
-	mkdir site_lisp
-
-site_lisp/htmlize.el: site_lisp
-	curl https://raw.githubusercontent.com/hniksic/emacs-htmlize/master/htmlize.el -o site_lisp/htmlize.el
--- publish.el
+++ publish.el
@@ -1,11 +1,8 @@
 (require 'org)
-(add-to-list 'load-path "./site_lisp")
-(require 'htmlize)
 (setq org-publish-project-alist
 	  '(("Memo"
 		 :base-directory "./src"
 		 :html-doctype "html5"
-		 :htmlized-source t
 		 :publishing-directory "./dist"
 		 :publishing-function org-html-publish-to-html
 		 :recursive t
@@ -13,10 +10,24 @@
 		 :with-author nil
 		 :with-date nil
 		 :with-timestamps nil
-		 :with-toc nil
+		 :with-toc t
 		 )))
 (setq org-publish-use-timestamps-flag nil)
 (setq org-html-postamble t)
+(setq org-html-head-include-scripts nil
+      org-html-head-include-default-style nil
+	  org-html-head "\
+<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />
+<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/styles/tokyo-night-dark.min.css\">\
+")
 (setq org-html-postamble-format
-	  '(("en" "<p class=\"date\">Date: %d</p>")))
+	  '(("en" "\
+<p class=\"date\">Date: %d</p>
+<script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js\"></script>
+<script>
+  document.querySelectorAll('.src').forEach((el) => {
+    hljs.highlightElement(el);
+  });
+</script>\
+")))
 (org-publish-project "Memo")
