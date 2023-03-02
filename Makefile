87ff4e5e11b7faac6b9d39ba52472920211782c2
yhiraki <coffexpr@gmail.com>
2023-03-02 10:23:16 +0900

    update

--- Makefile
+++ Makefile
@@ -1,6 +1,13 @@
 EMACS ?= emacs
 
-build:
+.PHONY: build
+build: dist
+
+dist: publish.el $(wildcard src/*.org) $(wildcard src/*/*.org)
 # emacs --batch is the one that inhibits the colorization of the code snippets.
 # so use -nw
 	TERM=xterm-256color $(EMACS) -Q -nw -l publish.el -f kill-emacs
+
+.PHONY: serve
+serve: dist
+	python3 -m http.server -d $<
--- publish.el
+++ publish.el
@@ -36,8 +36,8 @@
 	  '(("en" "\
 <p class=\"date\">Date: %d</p>
 <script src=\"https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.7.0/highlight.min.js\"></script>
-<link rel=\"stylesheet\" href=\"../static/css/main.css\" >\
-<script src=\"../static/js/main.js\"></script>
+<link rel=\"stylesheet\" href=\"/static/css/main.css\">
+<script src=\"/static/js/main.js\"></script>
 ")))
 
 (defvar org-babel-default-header-args:mermaid '((:results . "file") (:exports . "results")))
